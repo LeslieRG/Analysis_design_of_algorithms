f07583d5047c25c0692124ee2294b6f867cbd641
LeslieRG <lrgordian@gmail.com>
2018-06-18 16:29:21 -0400

    Updating Homework Controller

--- AsignacionesRina/Controllers/Homeworks.cs
+++ AsignacionesRina/Controllers/Homeworks.cs
@@ -1,11 +1,40 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using RinaLessons.DataAcces;
+using RinaLessons.Models;
 
 namespace RinaLessons.Controllers{
 
-
+    [Route ("api/v1/[controller]")]
     public class HomeworksController : Controller{
-        
-        
 
+        private readonly RinnaLessonsDbContext _context;
+        public HomeworksController (RinnaLessonsDbContext context){
+           _context = context;
+        }
+        
+        // [HttpGet("{id}")]
+
+        // public IActionResult GetAllHomeworks(int id)
+        // {
+
+        //     var task = _context.HomeWorks.ToList().Where(p => p.TeacherId == id);
+            
+        //     if()
+        //     {
+
+        //     }else if(task == null)
+        //     {
+        //         return Json (new Response{
+        //             message ="Solicitud",
+        //             info = "No hay tareas en esta materia"
+        //         }); 
+        //     }else 
+        //     {
+
+        //     }
+            
+        //     return Ok(_context.HomeWorks.ToList());
+        // }
     }
 }
